5b627d181b0fd80a7fee1b5fc89251f871567c2c
Joakim Borgersen <joakim.borgersen@nav.no>
2022-08-22 14:21:27 +0200

    Update dokmet poolsize

--- core/src/main/java/no/nav/dokmet/core/config/DokmetProperties.java
+++ core/src/main/java/no/nav/dokmet/core/config/DokmetProperties.java
@@ -52,9 +52,20 @@ public class DokmetProperties {
     @Data
     @Validated
     public static class Database {
-
+        /**
+         * Statisk pool verdi for dokkat databasen.
+         * <p>
+         * Optimizing UCP behaviour https://docs.oracle.com/database/121/JJUCP/optimize.htm#JJUCP8143
+         * About Optimizing Real-World Performance with Static Connection Pools
+         * https://docs.oracle.com/en/database/oracle/oracle-database/19/jjucp/optimizing-real-world-performance.html
+         * select STAT_NAME, to_char(VALUE) as VALUE, COMMENTS from v$osstat where stat_name IN ('NUM_CPUS','NUM_CPU_CORES','NUM_CPU_SOCKETS');
+         * Dokmet har et tak på 500 tilkoblinger
+         * Poolsize * max_pods må altså ikke overstige 500
+         * Current er satt til max 60 * 4 = 240
+         * @see no.nav.dokmet.core.repository.RepositoryConfig
+         */
         @Positive
-        private int poolsize = 20;
+        private int poolsize = 60;
     }
 
     @Data
