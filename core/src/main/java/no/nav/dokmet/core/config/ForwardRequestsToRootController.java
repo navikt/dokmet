e4666ea4914b78a830a05a3c88f82b297069e569
Christian Strandenæs <Christian.Fredrik.Osterby.Strandenes@nav.no>
2023-01-09 15:04:33 +0100

    MMA-6517 ForwardRequestsToRootController må være Unprotected

--- core/src/main/java/no/nav/dokmet/core/config/ForwardRequestsToRootController.java
+++ core/src/main/java/no/nav/dokmet/core/config/ForwardRequestsToRootController.java
@@ -1,8 +1,10 @@
 package no.nav.dokmet.core.config;
 
+import no.nav.security.token.support.core.api.Unprotected;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Unprotected
 @Controller
 public class ForwardRequestsToRootController {
 
