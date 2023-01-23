e9f6eb3d85a1d5f2565853f87e4ee945d6fd7406
Christian Strandenæs <Christian.Fredrik.Osterby.Strandenes@nav.no>
2023-01-23 13:34:02 +0100

    MMA-6632 la spring fikse LocalContainerEntityManagerFactoryBean

--- core/src/test/java/repository/config/RepositoryTestConfig.java
+++ core/src/test/java/repository/config/RepositoryTestConfig.java
@@ -9,31 +9,13 @@ import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 
 import javax.sql.DataSource;
+import java.util.HashMap;
 import java.util.Properties;
 
 import static org.flywaydb.core.api.MigrationVersion.fromVersion;
 
 @Configuration
 public class RepositoryTestConfig {
-
-	@Bean
-	public LocalContainerEntityManagerFactoryBean localContainerEntityManagerFactory(DataSource dataSource) {
-		LocalContainerEntityManagerFactoryBean factoryBean = new LocalContainerEntityManagerFactoryBean();
-		factoryBean.setPackagesToScan("no.nav.dokmet.core.domain.entities");
-		factoryBean.setDataSource(dataSource);
-		factoryBean.setPersistenceProvider(new HibernatePersistenceProvider());
-		factoryBean.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
-		Properties jpaProperties = new Properties();
-		jpaProperties.setProperty("hibernate.dialect", "org.hibernate.dialect.HSQLDialect");
-		jpaProperties.setProperty("hibernate.hbm2ddl.auto", "create");
-		jpaProperties.setProperty("hibernate.show_sql", "false");
-		jpaProperties.setProperty("hibernate.format_sql", "true");
-		jpaProperties.setProperty("hibernate.jdbc.fetch_size", "100");
-		jpaProperties.setProperty("hibernate.id.new_generator_mappings", "true");
-		jpaProperties.setProperty("hibernate.ejb.interceptor", "no.nav.dokmet.core.interceptor.ChangeStampInterceptor");
-		factoryBean.setJpaProperties(jpaProperties);
-		return factoryBean;
-	}
 
 	@Primary
 	@Bean(initMethod = "baseline")
