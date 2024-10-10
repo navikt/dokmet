fff152bd372d836c4ec0bf6b722da52cbb4fcd98
Christian Tumyr <christian.tumyr@nav.no>
2024-10-10 11:55:44 +0200

    MMA-7395 Legg til Flyway-script for xsd_fil-tabellen
    Co-authored-by: Anders Opskar Voldsund <anders.voldsund@nav.no>

--- core/src/main/java/no/nav/dokmet/core/domain/entities/XsdFil.java
+++ core/src/main/java/no/nav/dokmet/core/domain/entities/XsdFil.java
@@ -1,10 +1,12 @@
 package no.nav.dokmet.core.domain.entities;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Lob;
+import jakarta.persistence.SequenceGenerator;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -20,17 +22,24 @@ import lombok.Setter;
 @Entity
 @Table(name = "xsd_fil")
 public class XsdFil {
+
+	private static final String XSD_FIL_SEQ = "XSD_FIL_SEQ";
+
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = XSD_FIL_SEQ)
+	@SequenceGenerator(name = XSD_FIL_SEQ, sequenceName = XSD_FIL_SEQ, allocationSize = 1)
 	private Long id;
 
+	@Column(name = "brevpakke", nullable = false)
 	private String brevpakke;
 
+	@Column(name = "filsti", nullable = false)
 	private String filsti;
 
+	@Column(name = "filnavn", nullable = false)
 	private String filnavn;
 
+	@Column(name = "xsd_fil", nullable = false)
 	@Lob
 	private byte[] xsdfil;
-
 }
