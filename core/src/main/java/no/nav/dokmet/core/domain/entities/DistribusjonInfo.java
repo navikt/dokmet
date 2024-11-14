7cf282545503e7397d6a5ee89ecd2cc22405402e
Christian Tumyr <christian.tumyr@nav.no>
2024-11-14 09:35:01 +0100

    MMA-7715 Endre tosidigprint til default true
    Co-authored-by: Anders Opskar Voldsund <anders.voldsund@nav.no>

--- core/src/main/java/no/nav/dokmet/core/domain/entities/DistribusjonInfo.java
+++ core/src/main/java/no/nav/dokmet/core/domain/entities/DistribusjonInfo.java
@@ -103,8 +103,8 @@ public class DistribusjonInfo extends AbstractDomainObject {
 		this.sikkerhetsnivaa = sikkerhetsnivaa;
 	}
 
-	public boolean getTosidigPrint() {
-		return tosidigPrint == null ? true : tosidigPrint;
+	public Boolean getTosidigPrint() {
+		return tosidigPrint == null || tosidigPrint;
 	}
 
 	public SentralPrintDokumentTypeCode getSentralPrintDokumentType() {
